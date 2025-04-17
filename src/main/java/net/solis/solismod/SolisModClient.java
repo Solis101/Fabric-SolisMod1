00040c93f3cd1bd7576b6d2fcd49037bee7e4530
Solis101 <solis23042008@gmail.com>
2025-04-16 21:47:28 -0300

    test first commit

---
+++ src/main/java/net/solis/solismod/SolisModClient.java
@@ -0,0 +1,10 @@
+package net.solis.solismod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SolisModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
